377741e03897148f4cd81c1656f89ea9292aa885
chaowenguo0 <chaowen.guo1@gmail.com>
2025-10-28 20:09:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
-import streamlit as st
+import asyncio, aiohttp.web, math, pathlib, uvloop, os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+async def main():
+    app = aiohttp.web.Application()
+    app.add_routes([aiohttp.web.static('/', pathlib.Path(__file__).resolve().parent, show_index=True)])
+    runner = aiohttp.web.AppRunner(app)
+    await runner.setup()
+    site = aiohttp.web.TCPSite(runner, port=8501)
+    await site.start()
+    await asyncio.sleep(math.inf)
+
+uvloop.run(main())
