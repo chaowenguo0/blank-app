105f2c723545b8d11d2a73473452fa8b1a8562b0
chaowenguo0 <chaowen.guo1@gmail.com>
2025-10-28 20:15:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ async def main():
     app.add_routes([aiohttp.web.static('/', pathlib.Path(__file__).resolve().parent, show_index=True)])
     runner = aiohttp.web.AppRunner(app)
     await runner.setup()
-    site = aiohttp.web.TCPSite(runner, port=8501)
+    site = aiohttp.web.TCPSite(runner, host='127.0.0.1' ,port=8501)
     await site.start()
     await asyncio.sleep(math.inf)
 
